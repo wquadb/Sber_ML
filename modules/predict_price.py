373a23d0bdca96bb259156dfaa956b10b5fcc7a7
wquadb <esavdeev@edu.hse.ru>
2023-11-10 23:47:14 +0300

    writed preprocess at price

--- modules/predict_price.py
+++ modules/predict_price.py
@@ -1,10 +1,32 @@
 import process
-import pandas as pd
 
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch.functional as F
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+
+    """
+    takes DataFrame with column 'date' and change dates 
+    to the number of days from the earliest day
+    """
+
+    df['date'] = pd.to_datetime(df['date'], yearfirst=True, format=r'%d.%m.%Y')
+
+    df['date'] = (df['date'] - df['date'].min()).dt.days
+
+    df.rename({'date': 'day'}, axis=1, inplace=True)
+
+    return df
 
 def main():
 
     df = pd.read_csv('datasets/Predict_Future_Sales_sales_train.csv')
 
+    df = preprocess(df)
+
     print(df.head())
-    
+
+if __name__ == "__main__":
+    main()
